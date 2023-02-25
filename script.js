f1301c1e9b54ba74411be97ebc9d5af2ca6c3db1
Abstrakcionist <kondratev04g@mail.ru>
2023-02-25 17:33:14 +0500

    финальный вариант

--- script.js
+++ script.js
@@ -2,12 +2,33 @@
 function getRandomNumber(min, max) {
     return Math.round(Math.random() * ((max + 1) - min) + min);
 }
-let div = document.createElement('div');
-let sum = 0;
-let number = 0;
+
+
+//задержка при наводке
+function MakeRowHover(row) {
+
+    //добавление события наведения мыши
+    row.addEventListener("mouseover", function() {
+      setTimeout(() => this.style.backgroundColor = "silver", 200);
+    });
+
+    //добавление события отвода мыши
+    row.addEventListener("mouseout", function() {
+      this.style.backgroundColor = "";
+    });
+}
+
+
+let div = document.createElement('div');//добавление дива для суммы
+let sum = 0;//счетчик суммы
+let number = 0;//текущее случайное число
+
+
 //создание таблицы
 let table = document.createElement('table');
 document.body.appendChild(table);
+
+
 //заполнение таблицы
 for (let i = 0; i < 100; i++) {//создание 100 строк
 	let tr = document.createElement('tr');
@@ -21,8 +42,18 @@ for (let i = 0; i < 100; i++) {//создание 100 строк
 	let td2 = document.createElement('td');
     td2.innerHTML = number;
 	tr.appendChild(td2);
-	
+	//добавление строки в таблицу
 	table.appendChild(tr);
 }
-div.innerHTML = `Сумма всех чисел: ${sum}`;
+
+
+//реализация счетчика суммы случайных чисел
+div.innerHTML = `<p>Сумма всех чисел: ${sum}<p>`;
 document.body.appendChild(div);
+
+
+//реализация задержки при наведении
+let trs = document.querySelectorAll("tr");
+for (let i = 0; i < trs.length; i++) {
+  MakeRowHover(trs[i]);
+}
--- tests.js
+++
@@ -1,7 +0,0 @@
-let elem = document.body.querySelectorAll('.div');
-elem.onmouseover = function () {
-    elem.style.color = "red";
-};
-elem.onmouseleave = function () {
-    elem.style.color = "white";
-}
